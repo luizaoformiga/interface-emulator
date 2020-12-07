bf0adc2078886e8529ed4e61f97ca8132d7a3fa9
Luiz Lima <pereiralimaluizpaulo7@gmail.com>
2020-12-07 10:39:53 -0200

    adicionando animações

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # interface-emulator
+
+  Projeto desenvolvido por Luiz Lima, Treinando css e explorando o uso de jQuery.
+Apenas um treino com o tema da empresa de games NINTENDO, mostrando como exemplo alguns do jogos
+da franquia.
--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+//animation input
+$(() => {
+
+    $("input").on("focus", function(event) {
+        const div = $(this).parent(".input-field");
+        const label = div.children("label");
+
+        label.css("top", "-10px");
+    });
+
+    $("input").on("blur", function(event) {
+        const div = $(this).parent(".input-field");
+        const label = div.children("label");
+
+        if ($(this).val().length == 0) {
+            label.css("top", "12px");
+        }
+    });
+
+});
+
+//animation carousel
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
@@ -13,4 +35,4 @@ $('.owl-carousel').owlCarousel({
             items: 5
         }
     }
-})
+});
